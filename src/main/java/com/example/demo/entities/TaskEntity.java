add882880abc9458918351bca8cd321c7bea4f69
Yash Kambaria <yashkambaria245@gmail.com>
2024-08-30 17:35:32 +0530

    add task and list task working
    Signed-off-by: Yash Kambaria <yashkambaria245@gmail.com>

---
+++ src/main/java/com/example/demo/entities/TaskEntity.java
@@ -0,0 +1,14 @@
+package com.example.demo.entities;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data //this will automatically set getters and setters for each val
+public class TaskEntity {
+	private int id;
+	private String title;
+	private String description;
+	private Date deadline;
+	private boolean completed;
+}
